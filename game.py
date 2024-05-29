7b462b6e1cc4841216be98676899efe45439d69c
Daniel Dwulatka <138732225+DanhDie@users.noreply.github.com>
2024-05-29 18:39:28 -0300

    + descanso
    funcao de descanso
    +itens

--- game.py
+++ game.py
@@ -9,8 +9,10 @@ player = "X"
 op = 1
 tamanho = 1
 dificuldade = 0
+desafio = 0
+descanso = 0 
 
-inventario = ["Pocao de cura", "Escudo", "Bussola de escadas", "Bussola de desafios", "Bussola de tesouros"]
+inventario = ["Pocao de cura", "Escudo", "Bussola de escadas", "Bussola de desafios", "Bussola de tesouros", "Pocao de cura maior", "Travesseiro aconchegante", "Coracao implantavel", "Bussola de caminho seguro"]
 pontos = 0
 tipo=0
 #Variáveis de classe
@@ -29,6 +31,7 @@ vidaInimigo = 0
 dtInimigo = 0
 danoInimigo = 0
 modificadorInimigo=0
+
 #Menu
 
 def ApagarCs():
@@ -89,6 +92,8 @@ def GerarMapa(tamanho):
     global salas
     global mapa
     global player
+    global descanso
+    descanso = 0
     salas = []
     mapa = []
     
@@ -120,7 +125,6 @@ def ProximoAndar():
     print("PARABENS!!!\nAchou uma escada e desce para o proximo andar...")
     MostrarMapa()
     time.sleep(3)
-
 
 def MostrarMapa():   
 
@@ -174,6 +178,27 @@ def Mover():
             print("\nBateu a cara na parede!\n")
             time.sleep(2)
 
+def Descansar():
+    global descanso, vida, vidamax
+    ApagarCs()
+    op = int(input("Deseja descansar? (apenas uma vez por andar)\n\n1 - Sim  |  2 - Deixar pra depois\n\n"))
+    if(op==1):
+        if(descanso<1):
+            ApagarCs()
+            cura = (random.randint(1,12))+4
+            print("\nVocê descansa por um tempo e recupera",cura,"pontos de vida")
+            vida+=cura
+            if(vida>vidamax):
+                vida=vidamax
+            time.sleep(3)
+        else:
+            print("\nVocê nao pode mais descansar nesse andar")
+            time.sleep(3)
+    else:
+        print("...")
+        time.sleep(2)
+
+
 #Sessao de inventario e itens
 def MostrarInventario():
     global inventario
@@ -188,6 +213,7 @@ def MostrarInventario():
                 MostrarInventario()
             else:
                 UsarItem(aux)
+
 def UsarItem(item):
     global inventario
     match inventario[item]:
@@ -201,6 +227,14 @@ def UsarItem(item):
             RevelarTes(item)
         case "Bussola de escadas":
             RevelarEsc(item)
+        case "Pocao de cura maior":
+            PocaoVidaMais(item)
+        case "Travesseiro aconchegante":
+            Travi(item)
+        case "Coracao implantavel":
+            Coracao(item)
+        case "Bussola de caminho seguro":
+            RevelarCam(item)
         case _:
             print("Nada a se fazer...")
 
@@ -211,7 +245,23 @@ def PocaoVida(id):
         print("\nA vida está cheia!\n")
         time.sleep(3)
     else:
-        cura=RodarDado(10,5)
+        cura=RodarDado(8,3)
+        vida += cura
+        if(vida>vidamax):
+            vida = vidamax
+        inventario.pop(id)
+        ApagarCs()
+        print("\nCurou", cura, "Pontos de vida!1\n")
+        time.sleep(3)
+
+def PocaoVidaMais(id):
+    global vida, vidamax,inventario
+    if(vidamax==vida):
+        ApagarCs()
+        print("\nA vida está cheia!\n")
+        time.sleep(3)
+    else:
+        cura=RodarDado(12,8)
         vida += cura
         if(vida>vidamax):
             vida = vidamax
@@ -241,7 +291,7 @@ def RevelarEsc(id):
                 mapa[i][j]=9
     ApagarCs()
     print("A Bussula revela no seu mapa onde ficam as escadas no seu andar...")
-    print("Procure pelos NOVES")
+    print("\nProcure pelos NOVES")
     time.sleep(3)
 
 def RevelarTes(id):
@@ -252,7 +302,7 @@ def RevelarTes(id):
                 mapa[i][j]=7
     ApagarCs()
     print("A Bussula revela no seu mapa onde ficam os tesouros no seu andar...")
-    print("Procure pelos SETES")
+    print("\nProcure pelos SETES")
     time.sleep(3)
 
 def RevelarDes(id):
@@ -261,10 +311,56 @@ def RevelarDes(id):
         for j in range(tamanho):
             if(salas[i][j]==4):
                 mapa[i][j]=4
+    for i in range(tamanho):
+        for j in range(tamanho):
+            if(salas[i][j]==5):
+                mapa[i][j]=5
     ApagarCs()
     print("A Bussula revela no seu mapa onde ficam os desafios no seu andar...")
-    print("Procure pelos QUATROS")
+    print("\nProcure pelos QUATROS e CINCOS")
     time.sleep(3)
+
+def Travi(id):
+    ApagarCs()
+    global descanso, inventario
+    print("\nVocê se sente confortavel com esse travesseiro, pode descansar 1 vez a mais nesse andar")
+    descanso-=1
+    inventario.pop(id)
+    time.sleep(3)
+
+def Coracao(id):
+    ApagarCs()
+    global vida, vidamax, inventario
+    print("\nVocê percebe que esse Coração é um parasita vivo...")
+    time.sleep(3)
+    print("\nVocê aproxima ele de seu peito e ele fixa em você")
+    time.sleep(3)
+    print("\nMas você se sente bem!")
+    time.sleep(1)
+    print("\nGanha +10 de vida maxima")
+    time.sleep(3)
+    vida+=10
+    vidamax+=10
+    inventario.pop(id)
+    
+def RevelarCam(id):
+    for i in range(tamanho):
+        for j in range(tamanho):
+            if(salas[i][j]==1):
+                mapa[i][j]=1
+    for i in range(tamanho):
+        for j in range(tamanho):
+            if(salas[i][j]==2):
+                mapa[i][j]=2
+    for i in range(tamanho):
+        for j in range(tamanho):
+            if(salas[i][j]==3):
+                mapa[i][j]=3
+    ApagarCs()
+    print("A Bussula revela as salas seguras no seu andar...")
+    print("\nProcure pelos UNS, DOIS, TRES")
+    time.sleep(3)
+
 
 #Estatiticas
 
@@ -294,7 +390,9 @@ def AddPontos(pt):
 
 def PontoSala():
     global salas
-    AddPontos(salas[y][x])
+    if(salas[y][x]!=1):
+        AddPontos(salas[y][x])
+    
 
 #Nova seção de funções para combate
 def RolandoDados(vidaAtacado, dtAtacado, dadoMaximo, modificador, dif):
@@ -520,11 +618,15 @@ def Armadilha():
 #Desafios
 
 def DanoDesafio():
-    dano = random.randint(2,7)    
+    global vida
+    dano = random.randint(2,7)
+    vida -= dano
     print("\nPerde", dano, "pontos de Vida...")
     time.sleep(3)
 
 def Desafio():
+    global desafio
+    desafio = 0
     ApagarCs()
     print("Você entrou numa sala de desafio!\nPrepare-se...")
     time.sleep(2)
@@ -544,8 +646,15 @@ def Desafio():
             print("\nO jogo se chama VINTE E UM\nO objetivo do jogo é tirar o maximo de cartas possivel sem que a soma delas seja 21 ou maior, você pode decidir se deseja parar ou pegar outra carta, os valores são de 1 até 13")
             x = input("\nAperte ENTER se entendeu as regras ->")
             DesafioVinteUm()
+    if(desafio==1):
+        print("\nVocê encontra um item!")
+        time.sleep(2)
+        RecompensaDes()
+        print("Você ganhou 25 pontos por completar o desafio com sucesso!")
+        AddPontos(25)
 
 def DesafioMat():
+    global desafio
     ApagarCs()
     x = random.randint(10,20)
     y = random.randint(1,30)
@@ -583,14 +692,17 @@ def DesafioMat():
     resposta = int(input("\nQual é o resultado da equação:\n\n"+str(x)+" + "+str(y)+" x "+str(z)+" ?\n"))
     if(resposta==solucao):
         print("Resposta correta! Você provou seu valor, siga em frente!\n")
-        time.sleep(3)        
+        time.sleep(3)
+        desafio=1
+               
     else:
         print("Seu paspalhão! SUMA DAQUI\n")
         time.sleep(3)
         DanoDesafio()
+        desafio=-1
 
 def DesafioPPA():  
-    
+    global desafio
     escolha = int(input("\n1 - Pedra  |  2 - Papiro  |  3 - Adaga\n\n"))
     goblin = random.randint(1,3)
 
@@ -625,29 +737,36 @@ def DesafioPPA():
             print("\nVocê perdeu!\n\nO goblin ri de você, pega uma pedra, taca em você e sai correndo...\n")
             time.sleep(1)
             DanoDesafio()
+            desafio=-1
         else:
             print("\nVocê ganhou!\n\nO goblin sai correndo...\n")
             time.sleep(2)
+            desafio=1
     
     elif(escolha==2):
         if(goblin==3):
             print("\nVocê perdeu!\n\nO goblin ri de você, pega uma pedra, taca em você e sai correndo...\n")
             time.sleep(1)
             DanoDesafio()
+            desafio=-1
         else:
             print("\nVocê ganhou!\n\nO goblin sai correndo...\n")
             time.sleep(2)
+            desafio=1
     
     elif(escolha==3):
         if(goblin==1):
             print("\nVocê perdeu!\n\nO goblin ri de você, pega uma pedra, taca em você e sai correndo...\n")
+            desafio=-1
             time.sleep(1)
             DanoDesafio()
         else:
             print("\nVocê ganhou!\n\nO goblin sai correndo...\n")
+            desafio=1
             time.sleep(2)
 
 def DesafioQNumero():
+    global desafio
     print("\nUm Gato Magico Falante aparece e te fala que tem 7 chances para acertar a pergunta:\n")
     time.sleep(3)
     ratos = random.randint(1,100)    
@@ -674,12 +793,15 @@ def DesafioQNumero():
     if(chance==10):
         print("Parabens! Você acertou exatamente minhas refeições de hoje! Vou deixar você ir! miau\n")
         time.sleep(3)
+        desafio=1
     else:
         print("MEOOOWW!!! Você não tem intelecto sobre as refeições de um ser mistico como eu, não merece minha atenção...\n\nO gato te arranha e vai embora.")
         time.sleep(4)
         DanoDesafio()
+        desafio=-1
 
 def DesafioVinteUm():    
+    global desafio
     rodada = 1
     esqueleto = 0
     vc = 0
@@ -739,20 +861,97 @@ def DesafioVinteUm():
         print("\nMas te da uma rasteira e vc cai de cara no chão.")
         time.sleep(2)
         DanoDesafio()
+        desafio=-1
     elif(vc==esqueleto):
         print("\nEmpate!\nO esqueleto quer jogar de novo!")
         DesafioVinteUm()
     elif(vc>esqueleto or esqueleto>=21):
         print("\nVocê Ganhou! O esqueleto deixa você ir embora...")
         time.sleep(2)
+        desafio=1
     else:
         print("\nVocê Perdeu! O esqueleto deixa você ir embora...")
         time.sleep(2)
         print("\nMas te da uma rasteira e você cai de cara no chão.")
         time.sleep(2)
         DanoDesafio()
+        desafio=-1
         
-    
+#Recompensa
+
+def AtribuirItem(id):
+    global inventario
+    contador = 0
+    match id:
+        case 1:
+            print("\nAdquiriu uma Pocao de cura")
+            inventario.append("Pocao de vida")
+        case 2:
+            print("\nAdquiriu um Escudo")
+            for i in len(inventario):
+                if(inventario[i]=="Escudo"):
+                    contador+=1
+            if(contador==0):
+                inventario.append("Escudo")
+        case 3:
+            print("\nAdquiriu um Bussola de tesouros")
+            for i in len(inventario):
+                if(inventario[i]=="Bussola de tesouros"):
+                    contador+=1
+            if(contador==0):
+                inventario.append("Bussola de tesouros")
+        case 4:
+            print("\nAdquiriu um Bussola de escadas")
+            for i in len(inventario):
+                if(inventario[i]=="Bussola de escadas"):
+                    contador+=1
+            if(contador==0):
+                inventario.append("Bussola de escadas")
+        case 5:
+            print("\nAdquiriu um Bussola de desafios")
+            for i in len(inventario):
+                if(inventario[i]=="Bussola de desafios"):
+                    contador+=1
+            if(contador==0):
+                inventario.append("Bussola de desafios")
+        case 6:
+            print("\nAdquiriu um Pocao de vida maior")            
+            inventario.append("Pocao de vida maior")
+        case 7:
+            print("\nAdquiriu um Bussola de caminho seguro")
+            for i in len(inventario):
+                if(inventario[i]=="Bussola de caminho seguro"):
+                    contador+=1
+            if(contador==0):
+                inventario.append("Bussola de caminho seguro")
+        case 8:
+            print("\nAdquiriu um Travesseiro aconchegante")
+            inventario.append("Travesseiro aconchegante")
+        case 9:
+            print("\nConseguiu um Coracao implantavel")
+            inventario.append("Coracao implantavel")
+
+    time.sleep(3)
+    if(contador!=0):
+        print("\nJá possui esse item no inventario...")
+        time.sleep(2)
+
+def RecompensaDes():
+    x = random.randint(1,10)
+    if(x<=4):
+        AtribuirItem(1)
+    elif(x<=6):
+        AtribuirItem(6)
+    elif(x==7):
+        AtribuirItem(8)
+    elif(x==8):        
+        AtribuirItem(7)
+    elif(x==9):
+        AtribuirItem(4)
+    else:
+        AtribuirItem(5)
+
+
 Menu()
 GerarMapa(tamanho)
 
@@ -783,20 +982,23 @@ while(op!=0 and vida>0):
         op=Armadilha()
         salas[y][x]=1
 
-    if(salas[y][x]==4):
+    if(salas[y][x]==4 or salas[y][x]==5):
         PontoSala()
         Desafio()
         salas[y][x]=1
 
     else:    
-        op = int(input("O que quer fazer?\n\n0 - Encerrar  1 - Mover  2 - Inventario  3 - Status\n"))
+        op = int(input("O que quer fazer?\n\n0 - Encerrar  |  1 - Mover  |  2 - Inventario  |  3 - Status  |  4 - Descansar\n"))
         if(op==1):    
             Mover()
         if(op==2):    
             MostrarInventario()
         if(op==3):    
             MostrarEstatisticas()
+        if(op==4):
+            Descansar()
         else:
             print(...)
+            time.sleep(2)
 
 print("\nGAME OVER\n")
